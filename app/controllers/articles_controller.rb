40ac838d8e8dd900b681b7d491f9e51d542e2a7b
buddybuddymoon <buddybuddymoon@gmail.com>
2019-12-01 22:11:13 -0500

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,13 @@ class ArticlesController < ApplicationController
     end
     def new
         @article = Article.new
+
     end
 
     def show
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
     def create
         @article = Article.new(article_params)
